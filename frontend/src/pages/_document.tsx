18e0a53ac175433fce9df8310ee6a657ee0b49c8
TatsuyaSSK <ssktty514@gmail.com>
2024-10-25 21:46:01 +0900

    add OGP

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="/favicon.svg" />
+        <meta property="og:title" content="blanky" />
+        <meta
+          property="og:description"
+          content="blankyは簡単に自作の英語穴埋め問題が作れるアプリです"
+        />
+        <meta property="og:image" content="/ogp-cover.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
